039348965e8baf0f1c8735df5ee52ed3166f669f
nitishsharma25 <nitishsharma@microsoft.com>
2022-12-20 16:11:11 +0530

    test4

--- test-build.sh
+++ test-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-brew install gcc
+brew reinstall gcc
+brew install mysql
 pip3 install -r requirements.txt
 ls -latr /Users/runner/hostedtoolcache/Python/3.10.6/x64/lib/python3.10/site-packages/*
 python3 oracle_python_connector.py
