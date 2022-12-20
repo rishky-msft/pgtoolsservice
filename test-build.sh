db90dfa8dd5413f23580cbdfebd3ccfd60821e9e
nitishsharma25 <nitishsharma@microsoft.com>
2022-12-20 16:31:57 +0530

    test7

--- test-build.sh
+++ test-build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+virtualenv env1
+source env1/bin/activate
 pip3 install -r requirements.txt
 ls -latr /Users/runner/hostedtoolcache/Python/3.10.6/x64/lib/python3.10/site-packages/*
 python3 oracle_python_connector.py
