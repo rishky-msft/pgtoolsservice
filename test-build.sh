eedda2b37f2094cc358764a39c1e2ed953e5b01d
nitishsharma25 <nitishsharma@microsoft.com>
2022-12-20 16:02:27 +0530

    test3

--- test-build.sh
+++ test-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+brew install gcc
 pip3 install -r requirements.txt
 ls -latr /Users/runner/hostedtoolcache/Python/3.10.6/x64/lib/python3.10/site-packages/*
 python3 oracle_python_connector.py
